7acd6eab2e216120189d94f9b65b10f93f564a2c
Rakeshq <rakeshchhikara@gmail.com>
2021-05-18 23:12:32 -0700

    Almost done.. only getting EOF error after file copy. File is copied though

--- testings/show_tech_task.py
+++ testings/show_tech_task.py
@@ -1,6 +1,9 @@
-import csv
 from netmiko import ConnectHandler
-import getpass
+import paramiko
+from paramiko.client import SSHClient
+from scp import SCPClient
+import hashlib
+
 
 device_ip = input('Enter device ip: ')
 # password = getpass.getpass(prompt='Enter device Password: ')
@@ -20,6 +23,40 @@ hostname = prompt[12:-1]
 print(f'Generating show techs on device {hostname}. Please wait...')
 output = connection.send_command_expect('show tech-support', expect_string=r'available', max_loops=5000, delay_factor=5)
 print(output)
+
+location1 = output.find('CPU') + 7
+filename = output[location1:]
+
+print(f'\nFile to be copied: {filename}\n Copying file from device to this machine\n')
+check_md5 = connection.send_command_expect('show md5 file ' + filename, expect_string=r'available', max_loops=5000, delay_factor=5)
+
+md5_loc1 = check_md5.find('UTC') + 3
+remote_md5 = check_md5[md5_loc1:]
+print(f'MD5 hash value of file on Router: {remote_md5}')
+
+
+ssh = SSHClient()
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(device_ip, port=22, username='cisco', password='cisco')
+
+with SCPClient(ssh.get_transport()) as scp:
+    scp.get(filename)
+    scp.close
+
+
+md5_hash = hashlib.md5()
+with open(filename, "rb") as f:
+    # Read and update hash in chunks of 4K
+    for byte_block in iter(lambda: f.read(4096), b""):
+        md5_hash.update(byte_block)
+    md5_local = (md5_hash.hexdigest())
+    print(md5_local)
+
+if remote_md5 == md5_local:
+    print('MD5 has matched. Download successful')
+else:
+    print('MD5 has mismatch. Check again')
 
 connection.disconnect()
 print('Disconnected from device')
