944f736788e055caf9e8919d843ca5b3d3fef484
Rakeshq <rakeshchhikara@gmail.com>
2021-05-18 19:28:28 -0700

    show tech output successful

--- testings/show_tech_task.py
+++ testings/show_tech_task.py
@@ -15,7 +15,10 @@ device = {
 }
 
 connection = ConnectHandler(**device)
-output = connection.send_command('show tech')
+prompt = connection.find_prompt()
+hostname = prompt[12:-1]
+print(f'Generating show techs on device {hostname}. Please wait...')
+output = connection.send_command_expect('show tech-support', expect_string=r'available', max_loops=5000, delay_factor=5)
 print(output)
 
 connection.disconnect()
