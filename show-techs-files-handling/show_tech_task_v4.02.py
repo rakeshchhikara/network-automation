9f1c9986c8b0ce8da2741ee1659a730ab490085b
Rakeshq <rakeshchhikara@gmail.com>
2021-06-07 08:08:42 -0700

    Easy show-tech version 1.0
    3 features working condition

---
+++ show-techs-files-handling/show_tech_task_v4.02.py
@@ -0,0 +1,253 @@
+# Author: Rakesh Kumar (rakeshk6@cisco.com)
+# Usage: THIS SCRIPT WILL RUN SHOW-TECHS ON IOS-XR DEVICE AND THEN WILL COPY THE SHOW-TECH FILE TO LOCAL MACHINE.
+# ALSO IT WILL CHECK AND VERIFY REMOTE AND LOCAL(COPIED) FILE MD5 HASH VALUES.
+# FINALLY IT WILL UPLOAD THE DOWNLOADED FILES TO TAC CASE.
+# version 1
+import time
+from netmiko import ConnectHandler
+import paramiko
+from scp import SCPClient
+import hashlib
+import getpass
+import urllib3
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def run_cmd(user_cmd, hostname, connection):
+    print(f'\nGenerating "{user_cmd}" on device {hostname}. Please wait...\n')
+    output = connection.send_command(user_cmd, max_loops=50000, delay_factor=5)
+
+    if ('%' in output) or ('syntax error' in output):
+        print(f'Entered command - "{user_cmd}" is Invalid. Please re-check correct command.\n{120 * "#"}')
+    else:
+        print(output)
+        # Store show-tech file name and path on device in 'filename' variable.
+        file_start_loc = output.find('/harddisk')
+        file_end_loc = output.find('.tgz') + 4
+        filename = output[file_start_loc:file_end_loc]
+        return filename
+
+
+def retrieve_file(device_ip, port, username, password, filename):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(device_ip, port, username, password)
+    try:
+        scp = SCPClient(client.get_transport())
+        scp.get(filename)
+    except Exception as e:
+        print(f'Some Exception occurred while retrieving file to local machine. Exception detail:', e)
+    else:
+        print(f'\nFile "{filename}" copied to local machine\n')
+        local_file_name = filename[20:]  # Grabbing only name of file. Stripped path.
+        return local_file_name
+
+
+def remote_md5_check(connection, filename):
+    check_md5 = connection.send_command_expect('show md5 file ' + filename, max_loops=5000, delay_factor=5)
+
+    remote_md5 = check_md5[28:]
+    return remote_md5
+
+
+def local_md5_check(filename):
+    local_file_name = filename[20:]  # Grabbing only name of file. Stripped path.
+
+    # Calculating MD5 hash value of local file
+    md5_hash = hashlib.md5()
+    with open(local_file_name, "rb") as f:
+        # Read and update hash in chunks of 4K
+        for byte_block in iter(lambda: f.read(4096), b""):
+            md5_hash.update(byte_block)
+        md5_local = (md5_hash.hexdigest())
+        return md5_local
+
+
+def md5_compare(remote_md5, md5_local, local_files):
+    print(f'\nMD5 calculated on Device - {remote_md5}')
+    print(f'Local md5 calculated: {md5_local}')
+    if remote_md5 == md5_local:
+        print(f'MD5 has matched. Download successful.\n{120 * "#"}')
+        local_files.append(local_file_name)
+    else:
+        print(f'MD5 has mismatch. Check again.\n{120 * "#"}')
+
+
+def run_cmd_admin(user_cmd, hostname, connection, active_processor):
+        print(f'Entering in Admin mode to generate Admin specific show tech of command - "{user_cmd}"')
+
+        output = connection.send_command('admin', expect_string=r'sysadmin')
+        print(output)
+        print(f'\nGenerating "{user_cmd}" on device {hostname}. Please wait...\n')
+        output = connection.send_command(user_cmd, expect_string=r'sysadmin', max_loops=50000, delay_factor=5)
+
+        if ('%' in output) or ('syntax error' in output):
+            print(
+                f'Entered command - "{user_cmd}" is Incomplete or Invalid. Please re-check correct command.\n{120 * "#"}')
+        else:
+            print(output)
+            file_start_loc = output.find('/')
+            file_end_loc = output.find('.tgz') + 4
+            filename = output[file_start_loc:file_end_loc]
+            print('\nCopying file to Global mode')
+            copy_cmd = f'copy {filename} harddisk:/showtech location 0/{active_processor}/CPU0/VM1'
+            copy2global = connection.send_command(copy_cmd, expect_string=r'sysadmin')
+            print(f'{copy_cmd}/n{copy2global}')
+            connection.send_command('exit', expect_string=r'CPU')
+            file_loc = filename.find('showtech-')
+            filename = filename[file_loc:]
+            filename = '/harddisk:/showtech/' + filename
+            return filename
+
+
+
+
+
+def upload_2_sr(local_files, sr_username, sr_token, url):
+    for file in local_files:
+
+        auth = HTTPBasicAuth(sr_username, sr_token)
+        filename = file
+
+        f = open(filename, 'rb')
+        print(f'\nUploading file - "{filename}" to TAC Case\n')
+        r = requests.put(url + filename, f, auth=auth, verify=False)
+        r.close()
+        f.close()
+        if r.status_code == 201:
+            print(f'File - "{filename}" Uploaded Successfully to case.')
+        else:
+            print(f'File - "{filename}" upload to case Failed.')
+
+
+prompt_choices = '''
+To run show tech commands in "Global mode" and upload file to TAC case Choose: 1
+To run show tech commands in "Admin mode" and upload files to TAC case Choose: 2
+To upload already generated or saved file to TAC case Choose: 3
+'''
+
+print(prompt_choices)
+
+get_choice = int(input('Enter your Choice: '))
+
+# Get user input for TAC case details to upload files.
+url = 'https://cxd.cisco.com/home/'
+urllib3.disable_warnings()
+sr_username = input("Enter SR number: ")
+sr_token = input("Enter Upload Token: ")
+
+# Take user input for Device IP and Credentials.
+device_ip = input('Enter device ip: ')
+username = input('Enter your username for device Login: ')
+password = getpass.getpass(prompt='Enter device Password: ')
+
+local_files = []        # List to store file names.
+
+# Device details dict for connecting to device.
+device = {
+    'device_type': 'cisco_xr',
+    'host': device_ip,
+    'username': username,
+    'password': password,
+    'port': 22,  # optional, default 22
+    'verbose': True  # optional, default False
+}
+
+
+try:
+    if get_choice == 1:
+        print("\nuser selected option: 1\n")
+        # Get input from user for show tech commands to be run on device.
+        # Note, just Hit Enter if there is no more command for input.
+        lines = []
+        while True:
+            line = input('Enter command: ')
+            if line:
+                lines.append(line)
+            else:
+                break
+        text = '\n'.join(lines)
+
+        print(f'\nNow connecting to device...')
+        connection = ConnectHandler(**device)
+        prompt = connection.find_prompt()
+        prompt_strip = prompt.find(':') + 1
+        hostname = prompt[prompt_strip:-1]
+
+        for user_cmd in lines:
+            filename = run_cmd(user_cmd, hostname, connection)
+            if filename is None:
+                continue
+            remote_md5 = remote_md5_check(connection, filename)
+            local_file_name = retrieve_file(device_ip, '22', username, password, filename)
+            md5_local = local_md5_check(filename)
+            md5_compare(remote_md5, md5_local, local_files)
+
+        print(f'List of local files: {local_files}')
+
+        upload_2_sr(local_files, sr_username, sr_token, url)
+
+    elif get_choice == 2:
+        print("\nuser selected option: 2\n")
+        lines = []
+        while True:
+            line = input('Enter command (only admin mode commands without admin keyword): ')
+            if line:
+                lines.append(line)
+            else:
+                break
+        text = '\n'.join(lines)
+        print('\nNow will connect to Device...')
+
+        connection = ConnectHandler(**device)
+        prompt = connection.find_prompt()
+        prompt_strip = prompt.find(':') + 1
+        hostname = prompt[prompt_strip:-1]
+
+        loc1 = prompt.find('/CPU')
+        active_processor = prompt[5:loc1]
+
+        for user_cmd in lines:
+            filename = run_cmd_admin(user_cmd, hostname, connection, active_processor)
+            if filename is None:
+                continue
+            remote_md5 = remote_md5_check(connection, filename)
+            local_file_name = retrieve_file(device_ip, '22', username, password, filename)
+            md5_local = local_md5_check(filename)
+            md5_compare(remote_md5, md5_local, local_files)
+
+        print(f'List of local files: {local_files}')
+
+        upload_2_sr(local_files, sr_username, sr_token, url)
+
+    elif get_choice == 3:
+        print("\nUser selected option: 3\n")
+        lines = []
+        while True:
+            line = input('Enter filename with full-path: ')
+            if line:
+                lines.append(line)
+            else:
+                break
+        text = '\n'.join(lines)
+
+        connection = ConnectHandler(**device)
+        prompt = connection.find_prompt()
+        prompt_strip = prompt.find(':') + 1
+        hostname = prompt[prompt_strip:-1]
+
+        for filename in lines:
+            local_file_name = retrieve_file(device_ip, '22', username, password, filename)
+            remote_md5 = remote_md5_check(connection, filename)
+            md5_local = local_md5_check(filename)
+            md5_compare(remote_md5, md5_local, local_files)
+
+        print(f'List of local files: {local_files}')
+        upload_2_sr(local_files, sr_username, sr_token, url)
+
+    else:
+        print("Choose Option only from 1 to 3")
+except ValueError:
+    print("Enter only from option 1 to 3. Entered value not valid integer")
